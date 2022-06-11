e713cd74235104f4a6e84036d52ae5781e3f27be
choichoijin <cuj10594@naver.com>
2022-06-11 21:16:44 +0900

    css: 🎨 변수명 변경 및 스타일 지정

--- src/Slider.js
+++ src/Slider.js
@@ -2,41 +2,17 @@ import styled from "styled-components";
 import { useState } from "react";
 import { sliderInfo } from "./sliderInfo";
 
-let order = 1;
+let trackOrder = 1;
 
 function Slider() {
+  //한 트랙의 너비 = 이미지 너비 * 3
   const trackWidth = 1254;
+  //트릭 데이터로 인한 조정.
   const [style, setStyle] = useState({
-    //트릭 데이터로 인한 조정
     transform: `translateX(-${trackWidth}px)`,
   });
 
-  //order가 바뀌면 transform으로 렌더링.
-  // useEffect(() => {
-  //   console.log(order);
-  //   const moveWidth = order * trackWidth;
-  //   if (order === 0 || order === 1 || order === 2) {
-  //     moveStyle(moveWidth);
-  //   }
-
-  //   if (order < 0) {
-  //     setTimeout(() => {
-  //       setOrder(2);
-  //       setStyle({
-  //         transition: ``,
-  //       });
-  //       replaceSlide(moveWidth);
-  //     }, 1000);
-  //   }
-  //   if (order > 3) {
-  //     setOrder(1);
-  //     setStyle({
-  //       transition: ``,
-  //     });
-  //     setTimeout(() => replaceSlide(moveWidth), 1000);
-  //   }
-  // }, [order]);
-
+  //슬라이드 스타일 설정.
   function moveStyle(distance) {
     setStyle({
       transform: `translateX(-${distance}px)`,
@@ -44,6 +20,7 @@ function Slider() {
     });
   }
 
+  //트릭 데이터에서 진짜 데이터로 대체.
   function replaceSlide(distance) {
     setStyle({
       transform: `translateX(-${distance}px)`,
@@ -53,20 +30,20 @@ function Slider() {
   function handleOrder(direction) {
     //왼쪽 클릭시.
     if (direction === -1) {
-      order = order - 1;
+      trackOrder--;
     } else {
       //오른쪽 클릭시.
-      order = order + 1;
+      trackOrder++;
     }
-    moveStyle(order * trackWidth);
+    moveStyle(trackOrder * trackWidth);
 
     //무한 루프 핸들링.
-    if (order === 0) {
-      setTimeout(() => replaceSlide(order * trackWidth), 301);
-      order = 2;
-    } else if (order === 3) {
-      setTimeout(() => replaceSlide(order * trackWidth), 301);
-      order = 1;
+    if (trackOrder === 0) {
+      setTimeout(() => replaceSlide(trackOrder * trackWidth), 301);
+      trackOrder = 2;
+    } else if (trackOrder === 3) {
+      setTimeout(() => replaceSlide(trackOrder * trackWidth), 301);
+      trackOrder = 1;
     }
   }
 
@@ -80,11 +57,13 @@ function Slider() {
   return (
     <SliderArea>
       <h1>요즘 사람들이 좋아하는 공간의 비밀</h1>
-      <button onClick={() => handleOrder(-1)}>〈</button>
-      <button onClick={() => handleOrder(1)}>〉</button>
-      <SlideList>
-        <ul style={style}>{slideList}</ul>
-      </SlideList>
+      <Slides>
+        <button onClick={() => handleOrder(-1)}>〈</button>
+        <SlideList>
+          <ul style={style}>{slideList}</ul>
+        </SlideList>
+        <button onClick={() => handleOrder(1)}>〉</button>
+      </Slides>
     </SliderArea>
   );
 }
@@ -94,7 +73,9 @@ export default Slider;
 const SliderArea = styled.div`
   font-family: "GimhaeGayaB";
   h1 {
-    font-size: 2.5rem;
+    font-size: 36px;
+    margin: 20px;
+    text-align: center;
   }
 `;
 const SlideList = styled.div`
@@ -102,5 +83,26 @@ const SlideList = styled.div`
   overflow: hidden;
   ul {
     display: flex;
+  }
+
+  ul > li > p {
+    font-size: 15px;
+    margin: 8px;
+  }
+`;
+
+const Slides = styled.div`
+  display: flex;
+  justify-content: center;
+  button {
+    border: none;
+    width: 50px;
+    height: 50px;
+    margin: auto 10px;
+    border-radius: 15px;
+    cursor: pointer;
+  }
+  button:hover {
+    background-color: rgb(165, 165, 165);
   }
 `;
